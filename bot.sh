caf7b86fc6edb33dc12a9716aa2a4dfcf5a312ff
bhhoang <59357418+bhhoang@users.noreply.github.com>
2020-07-18 03:53:46 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,14 @@
+echo "Doi cai bot xong chi can go npm start"
+pkg update && pkg upgrade
+cd ..
+pkg install nodejs
+pkg install vim
+pkg install python
+pkg install python2
+pkg install git
+git clone -b 0.4.x-update https://github.com/lequanglam/c3c.git
+cd c3c
+npm i
+npm start
+
+echo "========= KET THUC  ========="

203a9ba085bcca12d7d3b5ebc66910c259c43e14
bhhoang <59357418+bhhoang@users.noreply.github.com>
2020-07-25 03:34:42 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -9,6 +9,9 @@ pkg install git
 git clone -b 0.4.x-update https://github.com/lequanglam/c3c.git
 cd c3c
 npm i
+npm i ffmpeg-static
+npm audit fix
+npm i deasync
 npm start
 
 echo "========= KET THUC  ========="
